436dab15751a8e059fa3189297a2a208ce47c21f
temesgen-0524 <temubre1996@gmail.com>
2026-03-14 09:36:58 +0300

    project closed for now

--- library_system/users/Auth.py
+++ library_system/users/Auth.py
@@ -3,29 +3,26 @@ import os
 import bcrypt
 from getpass import getpass
 
-USERS_FILE = "library_system/data/users.txt"
+USERS_FILE = "data/users.txt"
 
 def register(role="user"):
     username = input("Enter new username: ")
     password = getpass("Enter new password: ")
 
-    # Check if user already exists
     if os.path.exists(USERS_FILE):
-    # auth.py (inside register/login)
-# login()
         with open(USERS_FILE, "r") as f:
             for line in f:
                 parts = line.strip().split(",")
                 if len(parts) != 3:
-                    continue  # skip old/malformed lines
-                u, stored_hash, role = parts
-                if u == username and bcrypt.checkpw(password.encode('utf-8'), stored_hash.encode('utf-8')):
-                    print(f"Login successful! Welcome {username} ({role})")
-                    return role           
-    # Hash the password
+                    continue
+                u, stored_hash, stored_role = parts
+
+                if u == username:
+                    print("User already exists!")
+                    return False
+
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-    # Store username, hashed password, role
     with open(USERS_FILE, "a") as f:
         f.write(f"{username},{hashed},{role}\n")
 
